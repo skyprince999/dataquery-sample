039610e6db6555254615e292a01156816bed4f78
skyprince999 <aakashg80@gmail.com>
2025-02-08 19:48:56 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -74,42 +74,57 @@ def main():
   for (databases) in cursor:
       print(databases)
 
-  # #Populate Material code
-  # df = pd.read_excel("MatCat_Mapping_NOV2024.xlsx")
-  # print(f"Catalog : {df.shape}")
-  # print(f"Catalog cols: {df.columns}")
-  # df.to_sql("catalog", con=engine, if_exists="replace", index=False)
+  #Populate Material code
+  df = pd.read_excel("MatCat_Mapping_NOV2024.xlsx")
+  print(f"Catalog : {df.shape}")
+  print(f"Catalog cols: {df.columns}")
+  df.to_sql("catalog", con=engine, if_exists="replace", index=False)
 
-  # #Populate GRN data 
-  # df = pd.read_excel("grnreport_nov.XLSX")
-  # print(f"GRN : {df.shape}")
+  #Populate GRN data 
+  df = pd.read_excel("grnreport_nov.xlsx")
+  print(f"GRN : {df.shape}")
 
-  # df.rename(str.lower, axis='columns', inplace=True)
-  # for col in df.columns:
-  #   new_col = col.replace(" ", "_")
-  #   # print(col, new_col)
-  #   df.rename(columns={col: new_col}, inplace=True)
+  df.rename(str.lower, axis='columns', inplace=True)
+  for col in df.columns:
+    new_col = col.replace(" ", "_")
+    # print(col, new_col)
+    df.rename(columns={col: new_col}, inplace=True)
   
-  # df = financial_year(df, "document_date")
-  # df = map_catalog(df)
-  # print(f"GRN cols: {df.columns}")
-  # df.to_sql("grnreport", con=engine, if_exists="replace", index=False)
+  df = financial_year(df, "document_date")
+  df = map_catalog(df)
+  print(f"GRN cols: {df.columns}")
+  df.to_sql("grnreport", con=engine, if_exists="replace", index=False)
 
   #Populate Purchase data 
-
+  df = pd.read_excel("purchase_combine_withcatcode.xlsx")
+  print(f"PurchaseReport : {df.shape}")
+  print(f"PurchaseReport cols: {df.columns}")
+  df.to_sql("purchasereport", con=engine, if_exists="replace", index=False)
 
   #Populate Stock Statement data 
+  df = pd.read_excel("stock_nov_withcatcode.xlsx")
+  print(f"StockReport : {df.shape}")
+  print(f"StockReport cols: {df.columns}")
+  df.to_sql("stockreport", con=engine, if_exists="replace", index=False)
 
   
   #Populate Reservation data 
-
-
+  df = pd.read_excel("inventory_consumption_pattern_iv_NOV2024.xlsx")
+  print(f"IVReport : {df.shape}")
+  print(f"IVReport cols: {df.columns}")
+  df.to_sql("reservationreport", con=engine, if_exists="replace", index=False)
 
   #Populate TECO data 
-
+  df = pd.read_excel("tecoreport_nov.xlsx")
+  print(f"TECOReport : {df.shape}")
+  print(f"TECOReport cols: {df.columns}")
+  df.to_sql("tecoreport", con=engine, if_exists="replace", index=False)
 
   #Populate CLM data
-
+  df = pd.read_excel("clm_sample.xlsx")
+  print(f"CLMReport : {df.shape}")
+  print(f"CLMReport cols: {df.columns}")
+  df.to_sql("clmreport", con=engine, if_exists="replace", index=False)
 
   #Show existing tables
   query = "SELECT * FROM information_schema.tables WHERE table_schema = 'bpd';"
